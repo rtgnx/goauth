9906ef2a0d2b4be70669332191c66774951d6cdc
Adrian Cybulski <adrian@cybulski.cc>
2021-01-03 15:34:38 +0000

    build pam module only on linux

---
+++ auth_linux.go
@@ -0,0 +1,29 @@
+
+// PAM Authentication Backend
+type PAM struct {
+	Service string
+}
+
+// Authenticate against pam passwd daabase (Linux Only)
+func (p PAM) Authenticate(c Credentials) (bool, error) {
+
+	t, err := pam.StartFunc(p.Service, c.Login, func(s pam.Style, msg string) (string, error) {
+		switch s {
+		case pam.PromptEchoOff:
+			return c.Password, nil
+		case pam.PromptEchoOn, pam.ErrorMsg, pam.TextInfo:
+			return "", nil
+		}
+		return "", errors.New("Unrecognized PAM message style")
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	if err = t.Authenticate(0); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
